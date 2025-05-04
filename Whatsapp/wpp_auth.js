7c6807e2154825c2573b1f2290f032576235ae45
Povea <107443470+DPoveaa@users.noreply.github.com>
2025-05-04 17:22:59 -0300

    a

--- Whatsapp/wpp_auth.js
+++ Whatsapp/wpp_auth.js
@@ -1,5 +1,5 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
-const { sendQRToTelegram} = require('../Telegram/tl_auth');
+const { sendQRToTelegram } = require('../Telegram/tl_auth');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
 const { sendTelegramMessage } = require('../Telegram/tl_enviar');
@@ -7,7 +7,7 @@ const { sendTelegramMessage } = require('../Telegram/tl_enviar');
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
-console.log(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID);
+let qrFoiEscaneadoRecentemente = false;
 
 const client = new Client({
     authStrategy: new LocalAuth({ dataPath: './auth_data' }),
@@ -19,17 +19,24 @@ const client = new Client({
 
 client.on('qr', async (qr) => {
     console.log('[QR EVENT] Novo QR recebido');
+    qrFoiEscaneadoRecentemente = true;
     await sendQRToTelegram(qr, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID);
-    // Agendar lembrete após 1 minuto
+
+    // Lembrete se não escanear
     setTimeout(() => {
-        sendTelegramMessage('⚠️ Lembrete: O QR Code ainda não foi escaneado.', null, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID);
-    }, 60000); // 60 segundos
+        if (qrFoiEscaneadoRecentemente) {
+            sendTelegramMessage('⚠️ Lembrete: O QR Code ainda não foi escaneado.', null, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID);
+        }
+    }, 60000);
 });
 
 client.on('authenticated', async () => {
     console.log('[AUTHENTICATED] Autenticado com sucesso!');
-    await sendTelegramMessage('✅ WhatsApp autenticado com sucesso!', null, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID);
-    process.exit(0); // Fecha o script após autenticar
+    if (qrFoiEscaneadoRecentemente) {
+        await sendTelegramMessage('✅ WhatsApp autenticado com sucesso!', null, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID);
+        qrFoiEscaneadoRecentemente = false; // reseta a flag
+    }
+    process.exit(0);
 });
 
 client.on('auth_failure', async (msg) => {
@@ -40,7 +47,7 @@ client.on('auth_failure', async (msg) => {
 
 client.on('disconnected', async (reason) => {
     console.log('[DISCONNECTED]', reason);
-    await sendTelegramMessage(`🔴 Bot do WhatsApp foi desconectado. Motivo: *${reason}*`, null,TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID);
+    await sendTelegramMessage(`🔴 Bot do WhatsApp foi desconectado. Motivo: *${reason}*`, null, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID);
     console.log('Tentando reinicializar...');
     client.destroy().then(() => client.initialize());
 });
