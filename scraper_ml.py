3c98da9413cc7e55177a74eacc23c5aed83d5aa3
Povea <107443470+DPoveaa@users.noreply.github.com>
2025-05-05 04:11:17 -0300

    Update scraper_ml.py

--- scraper_ml.py
+++ scraper_ml.py
@@ -387,7 +387,7 @@ def get_product_details(driver, url, max_retries=3):
             )
 
             # Monta mensagem final
-            parts = [f"🔥 *{product_title[:150]}*"]
+            parts = [f"🟡 *Mercado Livre*", "", f"🔥 *{product_title[:150]}*"]
             if promotion_type:
                 parts.append(
                     f"{promotion_type} - *{discount_text.upper()}!* 📉"
