56ac09bc499595a58e71d4421f8f972ab08e5597
Povea <107443470+DPoveaa@users.noreply.github.com>
2025-07-24 20:01:50 -0300

    Create test_selenium.py

---
+++ test_selenium.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://www.google.com")
+print(driver.title)
+driver.quit()
